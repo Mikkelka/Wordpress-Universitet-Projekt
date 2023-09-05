3d2cc7e1eee7f9f1f38d395277dd6162d4fa19d2
Mikkel Kjær Andersen <35904625+Mikkelka@users.noreply.github.com>
2023-09-05 09:10:15 +0200

    Delete themes/fictional-block-theme directory
    fictional-block-theme directory

--- themes/fictional-block-theme/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '03ef8b5447ea5506f535');
--- themes/fictional-block-theme/build/slide.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'f22b5a301c44d953fa45');
